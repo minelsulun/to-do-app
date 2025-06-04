4d3499709d76c20ea0ab848a091fe223b215b018
minel <minelsulun.3@gmail.com>
2025-06-04 18:48:11 +0300

    fixed priority sort

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -19,7 +19,17 @@ try {
 
 
 export const getAllTasksQuery = () => {
-    return db.prepare(`SELECT * FROM tasks`).all();
+    return db.prepare(`
+        SELECT * FROM tasks
+        ORDER BY
+            CASE priority
+                WHEN 'high' THEN 1
+                WHEN 'medium' THEN 2
+                WHEN 'low' THEN 3
+                ELSE 4
+                END,
+            id DESC;
+    `).all();
 };
 
 export const getDoneTasksQuery = () => {
